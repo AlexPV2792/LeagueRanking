f8b9b4eb3b8edf4d769759db177ddf1cd48adb8d
Francisco Javier Miranda Rico <franciscorico@Franciscos-MacBook-Air.local>
2024-09-07 08:44:44 -0600

    [1001] The Problem feature branch

--- src/LeagueRanking.java
+++
@@ -1,5 +0,0 @@
-public class LeagueRanking {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
-}
---
+++ src/main/java/com/span/leagueranking/Main.java
@@ -0,0 +1,5 @@
+package main.java.com.span.leagueranking;
+
+public class Main {
+  
+}
